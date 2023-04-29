5a3c1227b8b79e96a01886d386105611099a0225
Mikhail Kondrashin <mkondrashin@gmail.com>
2023-04-29 13:18:37 +0300

    initial commit

---
+++ cmd/tmlist/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/mpkondrashin/tmlist/pkg/c1ews"
+	"github.com/mpkondrashin/tmlist/pkg/process"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const EnvPrefix = "TMLIST"
+
+const (
+	ConfigFileName = "config"
+	ConfigFileType = "yaml"
+)
+
+const (
+	flagAddress = "address"
+	flagAPIKey  = "api_key"
+	flagDir     = "dir"
+	flagExt     = "ext"
+	flagFile    = "file"
+	flagDryRun  = "dry"
+)
+
+func Configure() {
+	fs := pflag.NewFlagSet("", pflag.ExitOnError)
+	fs.String(flagAddress, "", "Cloud One Woekload Security entry point URL")
+	fs.String(flagAPIKey, "", "Cloud One API Key")
+	fs.Bool(flagDir, false, "Process directories list")
+	fs.Bool(flagFile, false, "Process file extensions list")
+	fs.Bool(flagExt, false, "Process file extensions list")
+	fs.Bool(flagDryRun, false, "Dyr run - do not modify existing lists")
+	err := fs.Parse(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := viper.BindPFlags(fs); err != nil {
+		log.Fatal(err)
+	}
+	viper.SetEnvPrefix(EnvPrefix)
+	viper.AutomaticEnv()
+
+	viper.SetConfigName(ConfigFileName)
+	viper.SetConfigType(ConfigFileType)
+	path, err := os.Executable()
+	if err == nil {
+		dir := filepath.Dir(path)
+		viper.AddConfigPath(dir)
+	}
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		_, ok := err.(viper.ConfigFileNotFoundError)
+		if !ok {
+			log.Fatal(err)
+		}
+	}
+}
+
+type (
+	List   func(context.Context) ([]c1ews.ListResponse, error)
+	Modify func(context.Context, int, *c1ews.List) (*c1ews.ListResponse, error)
+)
+
+func ProcessList(name string, list List, modify Modify, dryRun bool) {
+	log.Printf("%s: Start", name)
+	r, err := list(context.TODO())
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	p := process.NewProcess(r)
+	err = p.Process()
+	if err != nil {
+		log.Print(fmt.Errorf("%s: %w", name, err))
+		return
+	}
+	count := 0
+	p.IterateChanged(func(list *c1ews.ListResponse) error {
+		count++
+		log.Printf("%s: modify %s", name, list.Name)
+		if dryRun {
+			return nil
+		}
+		l := process.ListFromResponse(list)
+		_, err := modify(context.TODO(), list.ID, l)
+		return err
+	})
+	if count == 0 {
+		log.Printf("%s: No modifications", name)
+	}
+}
+
+func main() {
+	Configure()
+	host := viper.GetString(flagAddress)
+	if host == "" {
+		log.Fatal(fmt.Errorf("%s parameter is missing", flagAddress))
+	}
+	apikey := viper.GetString(flagAPIKey)
+	if apikey == "" {
+		log.Fatal(fmt.Errorf("%s parameter is missing", flagAPIKey))
+	}
+	ws := c1ews.NewWorkloadSecurity(apikey, host)
+	dryRun := viper.GetBool(flagDryRun)
+	all := !viper.GetBool(flagDir) && !viper.GetBool(flagExt) && !viper.GetBool(flagFile)
+	if viper.GetBool(flagDir) || all {
+		ProcessList("Directory Lists", ws.ListDirectoryLists, ws.ModifyDirectoryList, dryRun)
+	}
+	if viper.GetBool(flagExt) || all {
+		ProcessList("File Extension Lists", ws.ListFileExtensionLists, ws.ModifyFileExtensionList, dryRun)
+	}
+	if viper.GetBool(flagFile) || all {
+		ProcessList("File Lists", ws.ListFileLists, ws.ModifyFileList, dryRun)
+	}
+}
---
+++ pkg/process/list.go
@@ -0,0 +1,111 @@
+package process
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/mpkondrashin/tmlist/pkg/c1ews"
+	"golang.org/x/exp/maps"
+)
+
+// Equal - return true if there is a difference between lists.
+// We are checking only fileds that could be changed by TMList
+func Equal(a, b *c1ews.ListResponse) bool {
+	return a.Description == b.Description && reflect.DeepEqual(a.Items, b.Items)
+}
+
+// Includes - list all includes of the list
+func Includes(l *c1ews.ListResponse) (result []string) {
+	for _, line := range strings.Split(l.Description, "\n") {
+		colon := strings.Index(line, ":")
+		if colon == -1 {
+			continue
+		}
+		include := line[:colon]
+		if strings.ToLower(strings.TrimSpace(include)) != "include" {
+			continue
+		}
+		name := strings.TrimSpace(line[colon+1:])
+		result = append(result, name)
+	}
+	return
+}
+
+// HasIncludes - return true if list has includes
+func HasIncludes(l *c1ews.ListResponse) bool {
+	return len(Includes(l)) > 0
+}
+
+// Cleanup - remove all items if list has includes
+func Cleanup(l *c1ews.ListResponse) {
+	if HasIncludes(l) {
+		l.Items = []string{}
+	}
+}
+
+// AddToTheList - add items to the list avoiding duplicates and sort them
+func AddToTheList(l *c1ews.ListResponse, items []string) {
+	l.Items = RemoveDuplicates(append(l.Items, items...))
+}
+
+// ClearDependence - remove dependence lines from description if exist any
+func ClearDependence(l *c1ews.ListResponse) {
+	result := []string{}
+	for _, line := range strings.Split(l.Description, "\n") {
+		if strings.HasPrefix(line, DependencePrefix) {
+			continue
+		}
+		result = append(result, line)
+	}
+	l.Description = strings.Join(result, "\n")
+}
+
+// ListDependencies - return all dependencies for given list
+func ListDependencies(l *c1ews.ListResponse) (result []string) {
+	for _, line := range strings.Split(l.Description, "\n") {
+		if !strings.HasPrefix(line, DependencePrefix) {
+			continue
+		}
+		line = line[len(DependencePrefix):]
+		for _, name := range strings.Split(line, ",") {
+			name = strings.TrimSpace(name)
+			result = append(result, name)
+		}
+	}
+	return
+}
+
+// AddDependences - add dependence line or modify one of it already exists
+func AddDependences(l *c1ews.ListResponse, name ...string) {
+	if len(name) == 0 {
+		return
+	}
+	//fmt.Println("XXX AddDependences", strings.Join(name, "|"))
+	deps := ListDependencies(l)
+	ClearDependence(l)
+	deps = RemoveDuplicates(append(deps, name...))
+	dependence := fmt.Sprintf("\n%s %s", DependencePrefix, strings.Join(deps, ", "))
+	l.Description = l.Description + dependence
+}
+
+// RemoveDuplicates - remove duplicates from string slice. Return in sorted order
+func RemoveDuplicates(names []string) (result []string) {
+	m := make(map[string]struct{})
+	for _, each := range names {
+		m[each] = struct{}{}
+	}
+	result = maps.Keys(m)
+	sort.Strings(result)
+	return
+}
+
+// ListFromResponse - return List struct build from ListResponse struct
+func ListFromResponse(response *c1ews.ListResponse) *c1ews.List {
+	return &c1ews.List{
+		Name:        response.Name,
+		Description: response.Description,
+		Items:       response.Items,
+	}
+}
